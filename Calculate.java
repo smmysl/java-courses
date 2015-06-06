1f2dd40b2494c50fb9d344799a6b55ade5f7b8e7
smmysl <vasily@uliart.ru>
2015-06-06 23:19:04 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,13 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println ("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first+second;
+		int pro = first*second;
+		int raz = first-second;
+		System.out.println ("sum " + summ);
+		System.out.println ("proizv " + pro);
+		System.out.println ("raaznitsa " + raz);
 	}
 }
